2400e088bd643bf947d2dcc650f9923a189ee7c2
Josh Cox <josh@webhosting.coop>
2020-10-11 11:09:15 -0500

    hooks

--- lib/util.bash
+++ lib/util.bash
@@ -2,6 +2,9 @@
 : ${MIRROR_UPDATE_INTERVAL:=7}
 : ${SYSTEM_UPDATE_INTERVAL:=1}
 : ${SYSTEM_CLEARCACHE_INTERVAL:=30}
+: ${CLEARED_CACHE_MARKER:="/var/cache/pacman/.clearedcache"}
+: ${ROOT_UPDATED_MARKER:="/root/.updated"}
+: ${MIRROR_LIST_LOCATION:="/etc/pacman.d/mirrorlist"}
 
 . /etc/os-release
 if [[ $DEBUG == true ]]; then
@@ -10,11 +13,11 @@ fi
 
 pacman_clear_cache () {
   yes Y|sudo pacman -Scc
-  sudo touch "/root/.clearedcache"
+  sudo touch "$CLEARED_CACHE_MARKER"
 }
 
 update_mirrorlist () {
-  mirrorlist_file='/etc/pacman.d/mirrorlist'
+  mirrorlist_file="$MIRROR_LIST_LOCATION"
   sudo reflector \
       --threads 100 \
       --verbose \
@@ -25,7 +28,7 @@ update_mirrorlist () {
 }
 
 clear_all_placeholders () {
-  sudo rm -f "/root/.updated"
+  sudo rm -f "$ROOT_UPDATED_MARKER"
 }
 
 phile_czekr () {
@@ -46,7 +49,7 @@ phile_czekr () {
 }
 
 use_reflector () {
-  phile_czekr "/etc/pacman.d/mirrorlist" $MIRROR_UPDATE_INTERVAL update_mirrorlist
+  phile_czekr "$MIRROR_LIST_LOCATION" $MIRROR_UPDATE_INTERVAL update_mirrorlist
 }
 
 update_pacman () {
@@ -56,23 +59,39 @@ update_pacman () {
   else
     sudo pacman -Su --noconfirm
   fi
-  sudo touch "/root/.updated"
+  sudo touch "$ROOT_UPDATED_MARKER"
+}
+
+check_hooks () {
+  if [[ -f /etc/ssshutdown/hooks/in ]]; then
+    echo 'Found in hook'
+    sudo bash /etc/ssshutdown/hooks/in
+  fi
+}
+
+check_outhooks () {
+  if [[ -f /etc/ssshutdown/hooks/out ]]; then
+    echo 'Found out hook'
+    sudo bash /etc/ssshutdown/hooks/out
+  fi
 }
 
 pacman_update () {
   use_reflector
-  phile_czekr "/root/.clearedcache" $SYSTEM_CLEARCACHE_INTERVAL pacman_clear_cache
-  phile_czekr "/root/.updated" $SYSTEM_UPDATE_INTERVAL update_pacman
+  check_hooks
+  phile_czekr "$CLEARED_CACHE_MARKER" "$SYSTEM_CLEARCACHE_INTERVAL" pacman_clear_cache
+  phile_czekr "$ROOT_UPDATED_MARKER" "$SYSTEM_UPDATE_INTERVAL" update_pacman
+  check_outhooks
 }
 
 update_apt () {
   sudo apt-get update
   sudo apt-get upgrade -y
-  sudo touch "/root/.updated"
+  sudo touch "$ROOT_UPDATED_MARKER"
 }
 
 apt_update () {
-  phile_czekr "/root/.updated" $SYSTEM_UPDATE_INTERVAL update_apt
+  phile_czekr "$ROOT_UPDATED_MARKER" "$SYSTEM_UPDATE_INTERVAL" update_apt
 }
 
 try_update () {
